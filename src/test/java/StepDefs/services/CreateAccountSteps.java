70ebb71d182d13d59c493799544b8bd35e38f06a
sujith.s <sujith.s@ringcentral.com>
2024-08-29 12:14:10 +0530

    added naukri updating code

--- src/test/java/StepDefs/services/CreateAccountSteps.java
+++ src/test/java/StepDefs/services/CreateAccountSteps.java
@@ -34,25 +34,22 @@ public class CreateAccountSteps<IJavaScriptExecutor>
     public void iEnterValidDetailsAndGenerateAccount()throws InterruptedException {
         driver.findElement(By.xpath("//input[@id='usernameField']")).sendKeys("sayhitosujith@gmail.com");
         System.out.println("Enter the User Name");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//input[@id='passwordField']")).sendKeys("Qw@12345678");
         System.out.println("Enter the Password");
-        Thread.sleep(2000);
 
         driver.findElement(By.xpath("//button[@class='waves-effect waves-light btn-large btn-block btn-bold blue-btn textTransform']")).click();
         System.out.println("Click on login button");
         Thread.sleep(2000);
 
         driver.findElement(By.xpath("//div[@class='view-profile-wrapper']")).click();
-        System.out.println("Click on view profile");
         Thread.sleep(2000);
+        System.out.println("Click on view profile");
 
         WebElement upload_file = driver.findElement(By.xpath("//input[@value='Update resume']"));
         upload_file.click();
-        upload_file.sendKeys("resources//files//Resume.pdf");
+        upload_file.sendKeys("//Users//sujith.s//Downloads//Resume.pdf");
         System.out.println("upload resume");
-        Thread.sleep(2000);
 
         //get updated date
         WebElement updateddate = driver.findElement(By.xpath("//div[@class='updateOn typ-14Regular']"));
