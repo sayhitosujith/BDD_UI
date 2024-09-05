77ee6b30ab03689ebb1a45a8ba60be0343816f80
Naurki <sujithreddy1991@gmail.com>
2024-09-05 19:04:16 +0530

    added price sorting in amazon

--- src/test/java/StepDefs/services/CreateAccountSteps.java
+++ src/test/java/StepDefs/services/CreateAccountSteps.java
@@ -20,6 +20,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -255,15 +256,19 @@ public class CreateAccountSteps<IJavaScriptExecutor> {
 
     @And("I search Iphone{int}")
     public void iSearchIphone(int arg0) throws InterruptedException {
-        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Iphone12");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("iphone 12");
         driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        List<WebElement> iphone_price = Collections.singletonList(driver.findElement(By.xpath("//span[@class='a-price-whole']")));
-       for (int i=0;i<iphone_price.size();i++){
-          String text =  iphone_price.get(i).getText();
-           System.out.println(text);
-       }
+        List<WebElement> iphone_price = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
+        for (int i = 0; i < iphone_price.size(); i++) {
+            String text = iphone_price.get(i).getText();
+            System.out.println(text);
+        }
+        //string array
+        String[] iphone = {"54,899","51,999","54,899","51,99951","999,51","999,60","900,61","999,34","999,69","999,61","900","1,299","1,399","2,299","1,999"};
+        Arrays.sort(iphone);
+        System.out.println(Arrays.toString(iphone));
     }
 
     @Then("should Logout from Amazon successfully")
