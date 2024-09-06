d443ebb7c696cefdab9785399f0b9951dcd6fc6e
Naurki <sujithreddy1991@gmail.com>
2024-09-06 15:00:05 +0530

    updated

--- src/test/java/StepDefs/services/Practice/Example.java
+++ src/test/java/StepDefs/services/Practice/Example.java
@@ -1,13 +1,7 @@
 package StepDefs.services.Practice;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 
 public class Example {
     public static void main(String[] args) throws InterruptedException {
