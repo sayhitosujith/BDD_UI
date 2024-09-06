08813bf95ff2508391ba8de651e8ff5eb9c077f6
Naurki <sujithreddy1991@gmail.com>
2024-09-06 13:25:13 +0530

    added program to Retrieve all cookies

---
+++ src/test/java/StepDefs/services/Practice/CheckCookiesExample.java
@@ -0,0 +1,34 @@
+package StepDefs.services.Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Cookie;
+
+import java.util.Set;
+
+public class CheckCookiesExample {
+    public static void main(String[] args) {
+        // Set the path for the WebDriver executable (e.g., chromedriver)
+        System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to a website
+        driver.get("https://www.amazon.in/");
+
+        // Retrieve all cookies
+        Set<Cookie> cookies = driver.manage().getCookies();
+
+        // Print all cookies
+        for (Cookie cookie : cookies) {
+            System.out.println("Name: " + cookie.getName() +
+                    ", Value: " + cookie.getValue() +
+                    ", Domain: " + cookie.getDomain() +
+                    ", Path: " + cookie.getPath() +
+                    ", Expiry: " + cookie.getExpiry());
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
