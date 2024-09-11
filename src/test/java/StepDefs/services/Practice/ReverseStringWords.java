d0abcbbecc413b8daf17f72cac5a5045d4b71f99
Naurki <sujithreddy1991@gmail.com>
2024-09-11 16:46:24 +0530

    changed class names

--- src/test/java/StepDefs/services/Practice/ReverseStringWords.java
+++ src/test/java/StepDefs/services/Practice/ReverseStringWords.java
@@ -4,28 +4,25 @@ public class ReverseStringWords {
     public static String reversedWords(String str) {
         String[] words = str.split("");
 
-
         //initialise the string builder to store the result
         StringBuilder reversedString = new StringBuilder();
 
         for (int i = words.length - 1; i >= 0; i--) {
             reversedString.append(words[i]);
             if (i != 0) {
-                reversedString.append("");
             }
         }
 
-            return
-            reversedString.toString();
-        }
-
-
-        public static void main (String[] ars)
-        {
-            String input = "Tim is great";
-
-            String result = reversedWords(input);
-            System.out.println("Reversed:" + result);
-        }
+        return
+                reversedString.toString();
     }
+
+
+    public static void main(String[] ars) {
+        String input = "Tim is great";
+
+        String result = reversedWords(input);
+        System.out.println("Reversed:" + result);
+    }
+}
 
