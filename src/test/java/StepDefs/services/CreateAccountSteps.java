5a2e6a4d9d04e4504a85537889838d26db1543fd
sayhitosujith <sayhitosujith@gmail.com>
2025-02-01 17:19:20 +0530

    updated yaml

--- src/test/java/StepDefs/services/CreateAccountSteps.java
+++ src/test/java/StepDefs/services/CreateAccountSteps.java
@@ -8,11 +8,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import util.StepUtil;
 
