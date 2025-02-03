b2bfa6f4519a6554087f355bae6f83693ea644ac
Sadashivareddys <sujithreddy1991@gmail.com>
2025-02-03 17:12:59 +0530

    Updated browser versions

--- src/test/java/StepDefs/services/CreateAccountSteps.java
+++ src/test/java/StepDefs/services/CreateAccountSteps.java
@@ -10,7 +10,6 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
